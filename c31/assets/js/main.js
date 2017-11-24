b493d27a6b465a6f0105a76a4ab3e3375510d25c
Monzurul Islam <monzurul.ce.buet@gmail.com>
2017-11-24 18:49:37 +0600

    class 31 added

---
+++ c31/assets/js/main.js
@@ -0,0 +1,30 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+
+        var s = skrollr.init({
+        	render: function(data){
+
+        	}
+        });
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
