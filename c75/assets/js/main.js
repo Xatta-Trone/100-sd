b8dbd9013291afa0d7b4d05e52ff838d6c2fca56
Monzurul Islam <monzurul.ce.buet@gmail.com>
2018-03-17 09:36:38 +0600

    class 75 +76

---
+++ c75/assets/js/main.js
@@ -0,0 +1,28 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+
+        $('#bar1').barfiller();
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
